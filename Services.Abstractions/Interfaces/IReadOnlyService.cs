3fba25c1e5d2ef80f6e86d3eb392b43eefb755a5
Andrei Nikolaev <andrei.nikolaev@cmwp.ru>
2024-09-20 22:59:19 +0300

    Добавлен абстрактный слой для сервисов

---
+++ Services.Abstractions/Interfaces/IReadOnlyService.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="IReadOnlyService.cs" company="Andrey Nikolaev">
+// Copyright (c) Andrey Nikolaev. All rights reserved.
+// </copyright>
+
+namespace Services.Abstractions.Interfaces
+{
+    using Domain.Entities.Abstractions;
+
+    /// <summary>
+    /// Сервис по чтению данных из БД.
+    /// </summary>
+    /// <typeparam name="TEntity"> Тип сущности. </typeparam>
+    public interface IReadOnlyService<TEntity>
+        where TEntity : class, IIdEntity
+    {
+        /// <summary>
+        /// Получает сущность по идентификатору.
+        /// </summary>
+        /// <param name="id"> Идентификатор сущности. </param>
+        /// <returns> Найденная сущность. </returns>
+        public TEntity? GetId(Guid id);
+
+        /// <summary>
+        /// Получает асинхронно сущность по идентификатору.
+        /// </summary>
+        /// <param name="id"> Идентификатор сущности. </param>
+        /// <returns> Найденная сущность. </returns>
+        public Task<TEntity?> GetIdAsync(Guid id);
+
+        /// <summary>
+        /// Получение всех сущностей.
+        /// </summary>
+        /// <param name="track"> Статус необходимости отслеживания сущности. </param>
+        /// <returns> Коллекция сущностей. </returns>
+        public IQueryable<TEntity> GetAll(bool track = false);
+    }
+}
---
+++ Services.Abstractions/ReadOnlyService.cs
@@ -0,0 +1,53 @@
+﻿// <copyright file="ReadOnlyService.cs" company="Andrey Nikolaev">
+// Copyright (c) Andrey Nikolaev. All rights reserved.
+// </copyright>
+
+namespace Services.Abstractions
+{
+    using Domain.Entities.Abstractions;
+    using Microsoft.EntityFrameworkCore;
+    using Services.Abstractions.Interfaces;
+
+    /// <summary>
+    /// Сервис по чтению данных из БД.
+    /// </summary>
+    /// <typeparam name="TEntity"> Тип сущности. </typeparam>
+    /// <typeparam name="TDataContext"> Контекст базы данных. </typeparam>
+    public abstract class ReadOnlyService<TEntity, TDataContext> : IReadOnlyService<TEntity>
+        where TEntity : class, IIdEntity
+        where TDataContext : DbContext
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="ReadOnlyService{TEntity, TDataContext}"/>.
+        /// </summary>
+        /// <param name="dataContext"> Контекст бызы данных. </param>
+        public ReadOnlyService(TDataContext dataContext)
+        {
+            this.DataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+        }
+
+        /// <summary>
+        /// Контекст базы данных.
+        /// </summary>
+        protected TDataContext DataContext { get; }
+
+        /// <summary>
+        /// Получает все сущности.
+        /// </summary>
+        /// <param name="track"> Флаг отслеживания сущности. </param>
+        /// <returns> Перечисление объектов. </returns>
+        public abstract IQueryable<TEntity> GetAll(bool track = false);
+
+        /// <inheritdoc/>
+        public TEntity? GetId(Guid id)
+        {
+            return this.GetAll().SingleOrDefault(entity => entity.Id == id);
+        }
+
+        /// <inheritdoc/>
+        public async Task<TEntity?> GetIdAsync(Guid id)
+        {
+            return await this.GetAll().SingleOrDefaultAsync(entity => entity.Id == id);
+        }
+    }
+}
